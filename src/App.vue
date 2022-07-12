c6389b7f38eeeaa66e0caf4fd2304162cf8c5379
Ansel <ansel@zhtech.com.tw>
2022-07-12 08:51:05 +0800

    update home page

--- src/App.vue
+++ src/App.vue
@@ -36,13 +36,15 @@
 </template>
 
 <script>
+import food from './food.json'
+
 export default {
   name: "App",
 
   data: () => ({
-    tab: null,
-    items: ["Appetizers", "Entrees", "Deserts", "Cocktails"],
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      showSidebar: false,
+      inventory: food,
+      cart: {}
   }),
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,44 @@
 <template>
-  <div>
-    Home
+  <div class="home">
+    <div class="splash-container">
+      <div class="splash">
+        <h1>Splendid Food</h1>
+      </div>
+    </div>
+
+    <main class="wrapper">
+
+      <h2>Recommended</h2>
+
+      <div class="recommended">
+
+        <ProductCard
+          v-for="(product, index) in inventory.slice(0,3)"
+          :key="product.id"
+          class="card"
+          :index="index"
+          :product="product"
+        />
+
+      </div>
+
+    </main>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld.vue';
+import ProductCard from '@/components/ProductCard.vue'
+import food from '../food.json'
 
 export default {
   name: 'Home',
-  
-  components: {  },
-};
+  data: () => ({
+      inventory: food
+  }),
+  // props: ['inventory'],
+  components: {
+    ProductCard
+  }
+}
 </script>
